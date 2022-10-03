3a7fa5de1b0b198431ce77fd0de26a7b4e3b674c
Carlos Fuerte <113551817+CursedCola@users.noreply.github.com>
2022-10-03 19:53:56 +0000

    loops

--- loops.py
+++ loops.py
@@ -1,4 +1,10 @@
 def loops():
+  # a = 1
+  # while a < 10:
+ #   print(a)
+  
+
+
   
   ###################################loops intro######################################
   # queue videos
@@ -12,7 +18,8 @@ def loops():
   # For example: "Hello Norville"
   
   students = ["Norville", "Fred", "Velma", "Daphne"]
-  
+  # for students in students:
+  #   print(f"Hello {students} ")
   
   
   
@@ -20,8 +27,11 @@ def loops():
   # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  # sum_numbers = 
-  
+  # sum_numbers = 0
+  # for number in list_numbers:
+  #   # sum_numbers = sum_numbers + number
+  #   sum_numbers += number
+  #   print(sum_numbers)
   
   
   
@@ -30,7 +40,16 @@ def loops():
   # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
   
   list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
-  
+
+  sum_even = 0
+  sum_odd = 0
+  for number in list_numbers:
+    if number % 2 ==0:
+      sum_even =  sum_even + number
+      print(f"evens are {sum_even}")
+    else: 
+      sum_odd = sum_odd + number
+      print(f"evens are {sum_odd}")
   # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
   
   # num % 2 == 0 (even values)
